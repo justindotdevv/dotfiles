1dd984098f28e1757a7a9e88c24e7f40931e02c8
justindotdevv <justinomphithetse@gmail.com>
2026-04-18 21:11:51 +0200

    refactor(bufferline): overhaul for minimal look

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -4,14 +4,42 @@ return {
     require("bufferline").setup({
       options = {
         style = "minimal",
+        indicator = {
+          style = "none",
+        },
+        buffer_close_icon = "",
+        close_icon = "",
+        diagnostics = false,
+        always_show_bufferide = true,
+        separator_style = "thin",
       },
       highlights = {
         buffer_selected = {
           bg = "green",
           fg = "black",
+          bold = false,
+          italic = false,
+        },
+        buffer_visible = {
+          bg = "normal",
+          fg = "gray",
+        },
+        separator = {
+          fg = "bg",
+          bg = "bg",
+        },
+        separator_selected = {
+          fg = "bg",
+          bg = "bg",
+        },
+        separator_visible = {
+          fg = "bg",
+          bg = "bg",
+        },
+        fill = {
+          bg = "bg",
         },
       },
     })
   end,
 }
-
